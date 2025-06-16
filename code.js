f1a94f49dba015550a7305a527285adca216e5f1
Ducks <cansgamer@gmail.com>
2025-06-16 07:29:49 -0500

    aea

--- code.js
+++ code.js
@@ -1,15 +1,15 @@
 function randomString() {
-    const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-    //const characters = "1234567890";
-    let randomStr = '';
-        
-    for (let i = 0; i < 10; i++) {
-        randomStr += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
+  const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+  //const characters = "1234567890";
+  let randomStr = '';
 
-    console.log("L4D2#" + randomStr);
+  for (let i = 0; i < 10; i++) {
+    randomStr += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
 
-    return 'L4D2#' + randomStr;
+  console.log("L4D2#" + randomStr);
+
+  return 'L4D2#' + randomStr;
 }
 
 randomString();
